a6aae71e91324502969adfb08d41b8a000ff00b4
Bastian Haider <bastian.haider@gmx.at>
2021-11-03 13:30:41 +0100

    Backend added after gitignore

---
+++ ProchocBackend/ProchocBackend/Database/BasketProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace ProchocBackend.Database
+{
+    public class BasketProduct
+    {
+        public int Id { get; set; }
+        
+        public Basket Basket { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ ProchocBackend/ProchocBackend/Database/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ProchocBackend.Database
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; } 
+        [NotNull]
+        public string FirstName { get; set; }
+        [NotNull]
+        public string LastName { get; set; }
+        [NotNull]
+        public string Email { get; set; }
+    }
+}
