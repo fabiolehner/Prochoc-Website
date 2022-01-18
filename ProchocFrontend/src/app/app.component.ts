cd66f00d5baef5a46e36ac5216ec8b70a4f79147
Bastian Haider <bastian.haider@gmx.at>
2022-01-18 08:49:48 +0100

    changes in app component

--- ProchocFrontend/src/app/app.component.ts
+++ ProchocFrontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
+import { UserInfo } from './core/model/user_info';
+import { ConnectorService } from './core/service/connector.service';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @Component({
@@ -9,11 +11,22 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 export class AppComponent {
     title = 'prochoc-frontend';
 
+    constructor(private connector: ConnectorService) { }
+
     @ViewChild(ShoppingCartComponent) shoppingCart!: ShoppingCartComponent;
 
     toggleShoppingCart(): void {
         this.shoppingCart.refetch();
         this.shoppingCart.toggleShoppingCart();
+    }
+
+    isLoggedIn(): boolean {
+        return localStorage.getItem("__bearer") != undefined;
+    }
+
+    async getName(): Promise<String> {
+        var info = await this.connector.getUserInfo();
+        return info.firstName + " " + info.lastName;
     }
 }
 
